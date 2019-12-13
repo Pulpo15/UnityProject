012c9f425cb186b0ccf5b8712c1fe07f4e64488b
Pulpo <pol.gomar@enti.cat>
2019-12-14 00:36:04 +0100

    Turret Tracking
    Fix Enemy Death

--- UnityProject/Assets/Scripts/Enemy/EnemyController.cs
+++ UnityProject/Assets/Scripts/Enemy/EnemyController.cs
@@ -11,18 +11,35 @@ public class EnemyController : MonoBehaviour {
 
     void Start() {
         Enemy = GetComponent<CharacterController>();
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
     
     void Update() {
-        if (Turret == null)
-            Turret = GameObject.Find("PermanentTurret(Clone)");
 
         if (Turret != null) {
             transform.LookAt(Turret.transform);
             Enemy.Move(transform.forward * speed * Time.deltaTime);
         }
-
+        else if (Turret == null) {
+            transform.LookAt(Player.transform);
+            Enemy.Move(transform.forward * speed * Time.deltaTime);
+        }
         if (Input.GetKeyDown(KeyCode.Space))
             Destroy(Turret);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit) {
+        if (hit.gameObject.tag == "Bullet") {
+            Debug.Log("Enemy");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "PermanentTurret") {
+            Debug.Log("EnemyTurretCollider");
+            if (Turret == null)
+                Turret = GameObject.Find("PermanentTurret(Clone)");
+        }
     }
 }
