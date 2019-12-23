ba9abae984e4ab56d23418b36fa69296874fc1b1
Pulpo <pol.gomar@enti.cat>
2019-12-23 13:49:21 +0100

    EnemySpawner

---
+++ UnityProject/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    public GameObject EnemyPrefab;
+    public float time;
+    float curTime;
+
+    void Start() {
+        curTime = time;    
+    }
+
+    void Update() {
+
+        curTime -= Time.deltaTime;
+
+        if (curTime <= 0) {
+            SpawnEnemy();
+            curTime = time;
+        }
+    }
+
+    void SpawnEnemy() {
+        GameObject Enemy;
+        Enemy = Instantiate(EnemyPrefab,transform.position,Quaternion.identity);
+        Enemy.SetActive(true);
+    }
+}
